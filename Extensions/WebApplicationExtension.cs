25f283621e35c9176dee6c1423f334b6dca69044
Will Maynard <will@willmaynard.com>
2025-09-30 14:03:43 -0700

    Add FlexJson

--- Extensions/WebApplicationExtension.cs
+++ Extensions/WebApplicationExtension.cs
@@ -1,4 +1,5 @@
 using Maynard.Configuration;
+using Maynard.Json;
 using Maynard.Logging;
 using Microsoft.AspNetCore.Builder;
 
@@ -8,6 +9,20 @@ public static class WebApplicationExtension
 {
     public static WebApplication ConfigureMaynardTools(this WebApplication app, Action<PackageConfigurationBuilder> builder)
     {
+        FlexJson.Configure(logEvent =>
+        {
+            (logEvent.Severity switch
+            {
+                FlexJsonLogEventArgs.GOOD => Log.Good(logEvent.Message, logEvent.Data),
+                FlexJsonLogEventArgs.VERBOSE => Log.Verbose(logEvent.Message, logEvent.Data),
+                FlexJsonLogEventArgs.INFO => Log.Info(logEvent.Message, logEvent.Data),
+                FlexJsonLogEventArgs.WARN => Log.Warn(logEvent.Message, logEvent.Data),
+                FlexJsonLogEventArgs.ERROR => Log.Error(logEvent.Message, logEvent.Data),
+                FlexJsonLogEventArgs.ALERT => Log.Alert(logEvent.Message, logEvent.Data),
+                _ => Log.Error("Unexpected log severity!", new { LogEvent = logEvent })
+            }).Wait();
+        });
+        Log.Verbose("FlexJson log events captured and tied to logging.").Wait();
         builder.Invoke(new());
         Log.Good("Maynard Tools configured successfully!").Wait();
         return app;
