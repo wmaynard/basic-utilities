47bcb86234bcfa7f8897b126d18225f0dc9705b0
Will Maynard <will@willmaynard.com>
2025-10-03 16:57:12 -0700

    ConfigureAuth builder

--- Configuration/MaynardConfigurationBuilder.cs
+++ Configuration/MaynardConfigurationBuilder.cs
@@ -1,10 +1,14 @@
 using System.Reflection;
+using System.Security.Cryptography;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Maynard.Auth;
 using Maynard.Extensions;
 using Maynard.Json.Utilities;
 using Maynard.Logging;
 using Maynard.Singletons;
+using Maynard.Time;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,13 +26,47 @@ public class MaynardConfigurationBuilder : Builder
         Log.Good("Logging configured successfully!");
     });
 
+    public MaynardConfigurationBuilder ConfigureAuth(Action<AuthConfigurationBuilder> auth) => OnceOnly<MaynardConfigurationBuilder>(() =>
+    {
+        auth.Invoke(new());
+        if (string.IsNullOrWhiteSpace(JwtHelper.Config.ServerAudience))
+        {
+            Log.Warn("No audience specified; trying to use a default value from the HTTP context.");
+            HttpContextAccessor accessor = new();
+            JwtHelper.Config.ServerAudience = $"{accessor.HttpContext?.Request.Scheme}://{accessor.HttpContext?.Request.Host}";
+        }
+        if (string.IsNullOrWhiteSpace(JwtHelper.Config.ServerAudience))
+        {
+            Log.Warn("No issuer specified; trying to use a default value from the HTTP context.");
+            HttpContextAccessor accessor = new();
+            JwtHelper.Config.ServerIssuer = $"{accessor.HttpContext?.Request.Scheme}://{accessor.HttpContext?.Request.Host}";
+        }
+
+        if (string.IsNullOrWhiteSpace(JwtHelper.Config.PrivateKey) || string.IsNullOrWhiteSpace(JwtHelper.Config.PublicKey))
+        {
+            Log.Warn("A private or public key was not found for JWT authentication.  A new private / public key pair will be generated.  This is not recommended for production use.", new
+            {
+                Help = "Use the AuthConfigurationBuilder to set the private / public keys (PEM format).",
+                Detail = "Generated keys do not persist and are unique to each instance of the application.  Auth will work, but if deployed in a cluster with multiple instances, you will see auth errors."
+            });
+            JwtHelper.UseTemporaryKeyPair();
+        }
+
+        if (JwtHelper.Config.LifetimeInSeconds <= 0)
+        {
+            Log.Warn("Token TTL was not specified, or was invalid.  Defaulting to 1 hour.");
+            JwtHelper.Config.LifetimeInSeconds = Interval.OneHour;
+        }
+
+        if (JwtHelper.Config.CacheLifetimeInSeconds < 0)
+            JwtHelper.Config.CacheLifetimeInSeconds = 0;
+        JwtHelper.Config.Validate();
+    });
+
     public MaynardConfigurationBuilder ConfigureJsonSerialization(JsonOptions options) => OnceOnly<MaynardConfigurationBuilder>(() =>
     {
         JsonHelper.SerializerOptions.ApplyTo(options.JsonSerializerOptions);
     });
 }
-
-
-
 
 
