01f225958bf3488efc76d2e1652e9bcb6e829e72
Will Maynard <will@willmaynard.com>
2025-10-28 03:20:09 -0700

    0.8.6 | Updates to JwtAuthenticator to persist for IIS
    Hopeful fix for the IIS flicker we're seeing when apps are deployed

--- Web/JwtAuthenticator.cs
+++ Web/JwtAuthenticator.cs
@@ -1,51 +1,92 @@
+using System;
 using System.Security.Claims;
+using System.Threading.Tasks;
 using Maynard.Auth;
 using Maynard.Extensions;
 using Maynard.Logging;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Http;
 
 namespace Maynard.Web;
 
-public class JwtAuthenticator(IHttpContextAccessor context) : AuthenticationStateProvider
+public class JwtAuthenticator : AuthenticationStateProvider, IDisposable
 {
-    public string Id { get; } = Guid.NewGuid().ToString();
-    public TokenInfo _token;
+    private const string TokenStateKey = nameof(TokenStateKey);
+    private const string HasPersistedStateKey = nameof(HasPersistedStateKey);
+    private static readonly AuthenticationState _unauthenticatedState = 
+        new(new ClaimsPrincipal(new ClaimsIdentity()));
+
+    private readonly PersistentComponentState _persistentState;
+    private readonly IHttpContextAccessor _context;
+    private readonly IDisposable _persistingSubscription;
+
+    private TokenInfo _token;
+    private AuthenticationState _authState;
+
+    public JwtAuthenticator(IHttpContextAccessor context, PersistentComponentState persistentState)
+    {
+        _context = context;
+        _persistentState = persistentState;
+        _persistingSubscription = _persistentState.RegisterOnPersisting(PersistTokenAsync, RenderMode.InteractiveServer);
+    }
 
     public TokenInfo Token
     {
         get
         {
-            try
-            {
-                _token ??= context.ValidateJwt();
-            }
-            catch (Exception e)
-            {
-                Log.Error("Couldn't load token", e);
-            }
-
+            _token ??= FetchToken();
             return _token;
         }
-        private set => _token = value;
     }
 
-    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
     {
+        if (_authState == null)
+        {
+            _token ??= FetchToken();
+            _authState = _token?.ToAuthenticationState() ?? _unauthenticatedState;
+        }
+        return Task.FromResult(_authState);
+    }
+
+    private TokenInfo FetchToken()
+    {
+        if (_persistentState.TryTakeFromJson<TokenInfo>(TokenStateKey, out var persistedToken))
+        {
+            return persistedToken;
+        }
+
         try
         {
-            Token = context.ValidateJwt();
+            return _context.ValidateJwt();
         }
         catch
         {
-            Log.Error("Invalid or nonexistent JWT");
+            return null;
         }
-        
-        return Token?.ToAuthenticationState() 
-            ?? new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
-        // ClaimsPrincipal claims = CurrentToken?.ToClaims() ?? new ClaimsPrincipal(new ClaimsIdentity());
-        // return new AuthenticationState(new ClaimsPrincipal(claims));
+    }
+
+    private Task PersistTokenAsync()
+    {
+        // The OnPersisting callback can be triggered multiple times during the initial server render.
+        // We use HttpContext.Items to store a flag that is scoped to the specific HTTP request,
+        // ensuring we only persist the state once per request.
+        if (_context.HttpContext is { } httpContext && !httpContext.Items.ContainsKey(HasPersistedStateKey))
+        {
+            if (_token is not null)
+            {
+                _persistentState.PersistAsJson(TokenStateKey, _token);
+            }
+            httpContext.Items.Add(HasPersistedStateKey, true);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        _persistingSubscription.Dispose();
     }
 }
